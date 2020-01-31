e7cd3119186a795bb5d28fcdaf9509069c3e471b
KeremCanli <kerem_canl1@hotmail.com>
2020-01-31 22:57:01 +0300

    composer cache folder changed in php-fpm container

--- project/node.js/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* GET home page. */
-router.get("/", (request, response) => {
-    response.render("index", {title: "Express"});
-});
-
-module.exports = router;
--- project/node.js/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* GET users listing. */
-router.get("/", (request, response) => {
-    response.send("respond with a resource.");
-});
-
-module.exports = router;
